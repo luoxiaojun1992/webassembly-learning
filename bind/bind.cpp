f4232ceda4275c7d360e90450bc491c5d9a5ee8c
罗晓俊 <luoxiaojun1992@sina.cn>
2017-05-27 17:57:47 +0800

    add bind

--- bind/bind.cpp
+++ bind/bind.cpp
@@ -1,18 +1,37 @@
+// quick_example.cpp
 #include <emscripten/bind.h>
-#include <stdio.h>
 
 using namespace emscripten;
 
-float lerp(float a, float b, float t) {
-			printf("test");
-	    return (1 - t) * a + t * b;
-}
+class MyClass {
+public:
+  MyClass(int x, std::string y)
+    : x(x)
+    , y(y)
+  {}
 
-float addThree(int a) {
-  return 0;
-}
+  void incrementX() {
+    ++x;
+  }
 
-EMSCRIPTEN_BINDINGS(my_module) {
-	    function("lerp", &lerp);
-			function("addThree", &addThree);
+  int getX() const { return x; }
+  void setX(int x_) { x = x_; }
+
+  static std::string getStringFromInstance(const MyClass& instance) {
+    return instance.y;
+  }
+
+private:
+  int x;
+  std::string y;
+};
+
+// Binding code
+EMSCRIPTEN_BINDINGS(my_class_example) {
+  class_<MyClass>("MyClass")
+    .constructor<int, std::string>()
+    .function("incrementX", &MyClass::incrementX)
+    .property("x", &MyClass::getX, &MyClass::setX)
+    .class_function("getStringFromInstance", &MyClass::getStringFromInstance)
+    ;
 }
